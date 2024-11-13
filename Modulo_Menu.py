09b988cbcdc902bd80ce4e0da494a0b39a06393e
Bryan <b.charpentier.1@estudiantec.cr>
2024-11-13 02:45:05 -0600

    cambios

--- Modulo_Menu.py
+++ Modulo_Menu.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from Jugador import GestorJugadores, Jugador
+from Jugador import GestorJugadores
 import Modulo_Juego as juego
 import Modulo_Assets as assets
 
